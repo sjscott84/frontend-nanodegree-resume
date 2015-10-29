06961c9e89a1b1d307bfe37ac042d2527051fb14
Sarah Scott <s.j.scott84@gmail.com>
2015-10-29 15:40:50 -0700

    Create 4 new JSONs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,87 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio{
+	"name":"Sarah Joy Scott",
+	"role":"Web Developer",
+	"contacts":
+		{
+			"mobile":"925-913-0791",
+			"email":"s.j.scott84@gmail.com",
+			"github":"sjscott84",
+			"twitter":"N/A",
+			"location":"San Francisco, CA"
+		},
+	"welcomeMessage":"Hi!",
+	"skills":["Web Development", "Training", "Project Management"],
+	"biopic":images/Sarah.jpg,
+	//"display":function
+}
+
+var education{
+	"schools":[
+		{
+			"name":"University of Canterbury",
+			"location":"Christchurch, NZ",
+			"degree":"Bachelor of Arts",
+			"majors":"Art History",
+			"dates"://integer (graduation date),
+			//"url":"string" 
+		}
+	],
+	"onlineCourse":[
+		{
+			"title":
+			"school":
+			"date"://integer (date finished)
+			"url"://string
+		},
+		{
+			"title":
+			"school":
+			"date"://integer (date finished)
+			"url"://string
+		}
+	]
+	//display:function
+}
+
+var work{
+	"jobs":[
+		{
+			"employer":"TransGrid",
+			"title":"Project Services Officer",
+			"location":"Sydney, Australia",
+			"dates"://string (works with a hyphen between them),
+			"description":"Blah, blah, blah"
+		},
+		{
+			"employer":"Giraffe Learning Cente",
+			"title":"Office Manager",
+			"location":"Sydney, Australia",
+			"dates"://string (works with a hyphen between them),
+			"description":"Blah, blah, blah"
+		},
+		{
+			"employer":"Banque AIG",
+			"title":"Administrative Assistant",
+			"location":"London, England",
+			"dates"://string (works with a hyphen between them),
+			"description":"Blah, blah, blah"
+		}
+	]
+}
+
+var projects{
+	"projects":[
+		{
+			"title":"Project 1",
+			"dates":"string (works with a hyphen between them)",
+			"description":"Blah blah",
+			"images"://array with string urls
+		},
+		{
+			"title":"Project 2",
+			"dates":"string (works with a hyphen between them)",
+			"description":"Blah blah",
+			"images"://array with string urls
+		}
+	]
+}
